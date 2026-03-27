c47dc6d5897a13b3688d68f84e30385a5e8bcc90
Gracian Anton <gracian.anton@gmail.com>
2026-03-27 17:26:17 -0400

    commit

--- run_laravel_window_commands.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-C:\gracian\servers\wamp\bin\php\php8.4.0\php.exe C:\gracian\laravel-playground\artisan schedule:run
--- test.php
+++
@@ -1,2 +0,0 @@
-<?php
-file_put_contents(__DIR__ . "/text.txt", "Hello world\n");
