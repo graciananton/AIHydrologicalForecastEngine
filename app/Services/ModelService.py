55413804905a67a4fde641245e203b3b36114a9c
Gracian Anton <gracian.anton@gmail.com>
2026-04-29 09:36:26 -0400

    commit

--- app/Services/ModelService.py
+++ app/Services/ModelService.py
@@ -1,9 +1,9 @@
-
 from fastapi import FastAPI
 import numpy as np
 
 app = FastAPI()
 
-@app.post("/hello")
+@app.get("/hello")
 def predict(data: dict):
+    print("Received")
     return {"hello":"world"}
