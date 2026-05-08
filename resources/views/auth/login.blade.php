d8d0e5d03966de2eb18a63300ebc80269e9f4f97
Gracian Anton <gracian.anton@gmail.com>
2026-05-08 14:28:25 -0400

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,10 +9,12 @@
 </head>
 <body>
     <script>
-    window.__REACT_DATA__ = @json([
-                    ['error'=>session('error')],
-                    ['request'  => 'login']
-                    ]);
+        window.__REACT_DATA__ = @json(
+            [
+                ['error' => session('error')],
+                ['request'  => 'login']
+            ]
+        );
     </script>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <div id="react-root"></div>
