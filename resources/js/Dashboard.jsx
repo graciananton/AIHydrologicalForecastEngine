a7ea1be2f8b978bfd98cb00f4e8d713ff1553431
Gracian Anton <gracian.anton@gmail.com>
2026-05-08 18:59:44 -0400

    commit

--- resources/js/Dashboard.jsx
+++ resources/js/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 export default function Dashboard(){
     return (
-        <div id='dashboard' style={{position:"relative", border:"1px solid red", overflow:"auto", display:"flex", flexDirection:"column"}}>
+        <div id='dashboard' style={{position:"relative", border:"1px solid red", overflow:"auto", display:"flex", flexDirection:"column",width:"90%"}}>
             <div style={{display:"flex",flexDirection:"row",flex:1}}>
                 <div style={{flex:2, border:"1px solid black", textAlign:"center"}}>Station IDs</div>
                 <div style={{flex:1,border:"1px solid black", textAlign:"center"}}>02KF001</div>
@@ -15,35 +15,171 @@ export default function Dashboard(){
             </div>
             <div style={{display:"flex",flexDirection:"row",flex:1}}>
                 <div style={{display:"flex",flex:2, flexDirection:"row",border:"1px solid black",height:"300px",alignItems:"stretch"}}>
-                    <div style={{display:"flex",flex:1,border:"1px solid black",textAlign:"center",flexDirection:"column",justifyContent:"space-evenly"}}>
+                    <div style={{display:"flex",flex:1,border:"1px solid black",textAlign:"center",flexDirection:"column",justifyContent:"space-around"}}>
                         TEST
                     </div>
-                    <div style={{flex:1,border:"1px solid black",display:"flex", textAlign:"center",flexDirection:"column",justifyContent:"space-evenly"}}>
+                    <div style={{flex:1,border:"1px solid black",display:"flex", textAlign:"center",flexDirection:"column",justifyContent:"space-around"}}>
                         <div style={{border:"1px solid black"}}>RMSE (daily)</div>
                         <div style={{border:"1px solid black"}}>RMSE (weekly)</div>
                         <div style={{border:"1px solid black"}}>Last Updated</div>
                         <div style={{border:"1px solid black"}}>Graphs</div>
                     </div>
                 </div>
-                <div style={{flex:1,border:"1px solid black", textAlign:"center"}}>
-                    <div style={{display:"flex", textAlign:"center",flexDirection:"column",justifyContent:"space-evenly"}}>
-                        <div style={{border:"1px solid black"}}>RMSE (daily)</div>
-                        <div style={{border:"1px solid black"}}>RMSE (weekly)</div>
-                        <div style={{border:"1px solid black"}}>Last Updated</div>
-                        <div style={{border:"1px solid black"}}>Graphs</div>
-                    </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
                 </div>
-                <div style={{flex:1,border:"1px solid black", textAlign:"center"}}>02KF005</div>
-                <div style={{flex:1,border:"1px solid black", textAlign:"center"}}>02KF006</div>
-                <div style={{flex:1,border:"1px solid black", textAlign:"center"}}>02KF012</div>
-                <div style={{flex:1,border:"1px solid black", textAlign:"center"}}>02LA004</div>
-                <div style={{flex:1,border:"1px solid black", textAlign:"center"}}>02LA015</div>
-                <div style={{flex:1,border:"1px solid black", textAlign:"center"}}>02LA027</div>
-                <div style={{flex:1,border:"1px solid black", textAlign:"center"}}>02KF011</div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black",  whiteSpace: "nowrap"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black",  whiteSpace: "nowrap"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black",  whiteSpace: "nowrap"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
             </div>
-            <div>
+            <div style={{display:"flex",flexDirection:"row",flex:1}}>
+                <div style={{display:"flex",flex:2, flexDirection:"row",border:"1px solid black",height:"300px",alignItems:"stretch"}}>
+                    <div style={{display:"flex",flex:1,border:"1px solid black",textAlign:"center",flexDirection:"column",justifyContent:"space-around"}}>
+                        TRAIN
+                    </div>
+                    <div style={{flex:1,border:"1px solid black",display:"flex", textAlign:"center",flexDirection:"column",justifyContent:"space-around"}}>
+                        <div style={{border:"1px solid black"}}>Last Updated</div>
+                        <div style={{border:"1px solid black"}}>Graphs</div>
+                    </div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+            </div>
+            <div style={{display:"flex",flexDirection:"row",flex:1}}>
+                <div style={{display:"flex",flex:2, flexDirection:"row",border:"1px solid black",height:"300px",alignItems:"stretch"}}>
+                    <div style={{display:"flex",flex:1,border:"1px solid black",textAlign:"center",flexDirection:"column",justifyContent:"space-around"}}>
+                        FUTURE
+                    </div>
+                    <div style={{flex:1,border:"1px solid black",display:"flex", textAlign:"center",flexDirection:"column",justifyContent:"space-around"}}>
+                        <div style={{border:"1px solid black"}}>Predictions (daily)</div>
+                        <div style={{border:"1px solid black"}}>Predictions (weekly)</div>
+                        <div style={{border:"1px solid black"}}>Last Updated</div>
+                        <div style={{border:"1px solid black"}}>Graphs</div>
+                    </div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
+                <div style={{display:"flex", flexDirection:"column", justifyContent:"space-around", textAlign:"center", flex:1,border:"1px solid black"}}>
+                    <div style={{border:"1px solid black"}}>0.23343233232</div>
+                    <div style={{border:"1px solid black"}}>0.323232</div>
+                    <div style={{border:"1px solid black"}}>2026-05-08 14:32</div>
+                    <div style={{border:"1px solid black"}}>Graphs</div>
+                </div>
 
             </div>
+
         </div>
     )
 }
