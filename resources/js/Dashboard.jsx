e6da144f780e9d22669a1be9af962ece269bc7f2
Gracian Anton <gracian.anton@gmail.com>
2026-05-11 12:29:48 -0400

    commit

--- resources/js/Dashboard.jsx
+++ resources/js/Dashboard.jsx
@@ -1,55 +1,55 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import '../css/Dashboard.css';
-function Stations(){
-    const url = "https://gracian.ca/laravel/public/api/stations";
 
-    const [stations, setStations] = useState([]);
+function Stations({stations, setStations}){
+    const url = "https://gracian.ca/laravel/public/api/stations";
 
     useEffect(() => {
         const fetchStations = async () => {
-            const stations = await fetch(url);
-            const json_stations = await stations.json();
+            const list_stations = await fetch(url);
+            const json_stations = await list_stations.json();
             setStations(json_stations);
         }
         fetchStations();
     },[])
 
-    console.log(stations);
     return (
         <div id='stations'>
             <div>Station IDs</div>
-            <div>02KF001</div>
-            <div>02KF005</div>
-            <div>02KF006</div>
-            <div>02KF012</div>
-            <div>02LA004</div>
-            <div>02LA015</div>
-            <div>02LA027</div>
-            <div>02KF011</div>
+            {stations.map(station => (
+                <div>{station.stationId}</div>
+            ))}
         </div>
     )
-    
 }
-
+function TableSection(section, categories){
+    {console.log(categories)}
+    return (
+        <div>
+            <div>{section}</div>
+            <div>
+            {categories.map((category, index) => (
+                <div>{category}</div>
+            ))}
+            </div>
+        </div>
+    )
+}
 export default function Dashboard(){
     const [showPopup, setShowPopup] = useState(false);
+    const [stations, setStations] = useState([]);
     return (
         <div id='dashboard'>
             <div id='datasets'>
-                <Stations />
+                <Stations 
+                    stations = {stations}
+                    setStations = {setStations}
+                />
+
                 <div id='test'>
-                    <div>
-                        <div>
-                            TEST
-                        </div>
-                        <div>
-                            <div>RMSE (daily)</div>
-                            <div>RMSE (weekly)</div>
-                            <div>Last Updated</div>
-                            <div>Graphs</div>
-                        </div>
-                    </div>
+                    {console.log(stations)}
+                    <TableSection section='TEST' categories={['RMSE(daily)','RMSE(weekly)','Last Updated','Graphs']} />
                     <div>
                         <div>0.23332</div>
                         <div>0.323232</div>
@@ -120,15 +120,7 @@ export default function Dashboard(){
                 </div>
 
                 <div id='train'>
-                    <div>
-                        <div>
-                            TRAIN
-                        </div>
-                        <div>
-                            <div>Last Updated</div>
-                            <div>Graphs</div>
-                        </div>
-                    </div>
+                    <TableSection section='TRAIN' categories={['Last Updated','Graphs']} />
                     <div>
                         <div>2026-05-08 14:32</div>
                         <div>Graphs</div>
@@ -163,17 +155,7 @@ export default function Dashboard(){
                     </div>
                 </div>
                 <div id='future'>
-                    <div>
-                        <div>
-                            FUTURE
-                        </div>
-                        <div>
-                            <div>Predictions (daily)</div>
-                            <div>Predictions (weekly)</div>
-                            <div>Last Updated</div>
-                            <div>Graphs</div>
-                        </div>
-                    </div>
+                    <TableSection section='FUTURE' categories={['Predictions(daily)','Predictions (weekly)','Last Updated','Graphs']} />
                     <div>
                         <div>0.23332</div>
                         <div>0.323232</div>
