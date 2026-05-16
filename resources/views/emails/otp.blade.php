7ac5d7b6f927ee9fc70e7fe3c05902a9e8086570
Gracian Anton <gracian.anton@gmail.com>
2026-05-16 19:14:22 -0400

    commit

--- resources/views/emails/otp.blade.php
+++ resources/views/emails/otp.blade.php
@@ -1,1 +1,17 @@
-Verification Code: {{$otp}}
+<p>
+    Your verification code for the
+    <b>AI Hydrological Forecasting System</b>
+    is:
+</p>
+<p>
+    <span style="font-size:0.8rem;">
+        {{ $otp }}
+    </span>
+</p>
+<p>
+    This is your one-time verification code.
+    The code is only valid for 5 minutes.
+</p>
+<p>
+    Thanks.
+</p>
