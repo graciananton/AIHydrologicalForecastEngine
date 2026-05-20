fc3283024bdbca320eec80b050433c6faae3cd63
Gracian Anton <gracian.anton@gmail.com>
2026-05-20 10:06:03 -0400

    commit

--- resources/views/auth/verificationCode.blade.php
+++ resources/views/auth/verificationCode.blade.php
@@ -5,7 +5,6 @@
         'resources/css/app.css',
         'resources/js/app.jsx'
     ])
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
 </head>
 <body>
     <script>
