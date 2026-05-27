c300f80eecf5658873080b9d104b809639e07def
Gracian Anton <gracian.anton@gmail.com>
2026-05-27 14:52:49 -0400

    commit

--- resources/views/user/station.blade.php
+++ resources/views/user/station.blade.php
@@ -1,6 +1,11 @@
 <!doctype html>
 <html>
 <head>
+    @vite([
+        'resources/css/app.css',
+        'resources/js/app.jsx'
+    ])
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
 </head>
 <body>
@@ -17,10 +22,5 @@
     </script>
 
     <div id="react-root"></div>
-    
-    @vite([
-        'resources/css/app.css',
-        'resources/js/app.jsx'
-    ])
 </body>
 </html>
