017c29dd9b66c2f979011514087f5c88d4307773
Gracian Anton <gracian.anton@gmail.com>
2026-05-27 13:42:09 -0400

    commit

--- app/Http/Controllers/UserStationController.php
+++ app/Http/Controllers/UserStationController.php
@@ -7,23 +7,14 @@ use App\Services\UserStationService;
 class UserStationController extends Controller
 {
     public function __construct(Request $request){
+    
     }
     public function process(UserStationService $userStationService){
         $user = $userStationService->getUser(session($email));
-        if($user){
-            return view("user.station", 
-            [
-                ['request' => 'userStation'],
-                ['email' => session('email')],
-                ['stationId' => $user->stationId]
-            ]
-            );
-        }
         return view("user.station", 
             [
                 ['request' => 'userStation'],
-                ['email' => session('email')],
-                ['stationId' => null]
+                ['stationId' => ($user) ? $user->stationId : null]
             ]
         );
 
