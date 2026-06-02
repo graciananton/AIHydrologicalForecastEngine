a90e0db918280e0f91a493720f56770cdfa6fe71
Gracian Anton <gracian.anton@gmail.com>
2026-06-02 14:45:26 -0400

    commit

--- app/Services/StatsService.php
+++ app/Services/StatsService.php
@@ -19,14 +19,35 @@ class StatsService
         $currentHour = Carbon::now('UTC')->startOfHour();
         $futureHour = Carbon::now('UTC')->startOfHour()->addHours(24);
 
-        $prediction = Predictions::where('stationId', $params['stationId'])
+        $predictions = Predictions::where('stationId', $params['stationId'])
+            ->whereBetween('predictedFor', [$currentHour, $futureHour])
+            ->orderBy('prediction', 'desc')
+            ->get();
+
+        $maxPrediction = Predictions::where('stationId', $params['stationId'])
+            ->whereBetween('predictedFor', [$currentHour, $futureHour])
+            ->orderBy('prediction', 'desc')
+            ->first();
+
+        $minPrediction = Predictions::where('stationId', $params['stationId'])
             ->whereBetween('predictedFor', [$currentHour, $futureHour])
             ->orderBy('predictedFor','asc')
-            ->max('prediction');
-    
+            ->first();
+
+        $currentLevel = Predictions::where('stationId', $params['stationId'])
+            ->where('predictedFor', $currentHour)
+            ->first();
         
-
-
-        return null;
+        
+        return [
+            'currentLevel' => $currentLevel->prediction,
+            'trend' => ($predictions[count($predictions)-1]->prediction - $predictions[0]->prediction > 0.0) ? "rising": "falling",
+            'lastUpdated' => $prediction[count($predictions)-1]->updated_at,
+            'maximumForecast' => $maxPrediction->prediction,
+            'minForecast' => $minPrediction->prediction,
+            'maximumForecast' => $maxPrediction->prediction,
+            'peakTime' => $maxPrediction->updated_at,
+            'change' => $predictions[count($predictions)-1]->prediction - $predictions[0]->prediction
+        ];
     }   
 }
