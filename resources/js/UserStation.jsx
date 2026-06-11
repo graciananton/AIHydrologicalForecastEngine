7da0a7ac6611c5556b788f920ea92e8c288689b1
Gracian Anton <gracian.anton@gmail.com>
2026-06-11 17:29:11 -0400

    commit

--- resources/js/UserStation.jsx
+++ resources/js/UserStation.jsx
@@ -5,6 +5,5 @@ export default function UserStation(){
     const data = window.__REACT_DATA__;
     console.log("User Station default function");
     console.log(data);
-    
     return ("Hello");
 }
