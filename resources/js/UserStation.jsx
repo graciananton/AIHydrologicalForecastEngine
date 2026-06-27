214eb3e30566d4af3082980b409bbeb4461e4e8d
Gracian Anton <gracian.anton@gmail.com>
2026-06-27 17:39:08 -0400

    commit

--- resources/js/UserStation.jsx
+++ resources/js/UserStation.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-//import '../css/UserStation.css';
+import '../css/UserStation.css';
 
 export default function UserStation(){
     const data = window.__REACT_DATA__;
-    console.log("User Station default function");
     console.log(data);
     return ("Hello");
+}
+function Main(){
+    return (
+        <div id='main'>
+
+        </div>
+    )
 }
