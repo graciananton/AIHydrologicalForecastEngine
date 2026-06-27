1e34329a023c406c270b83f7b7c80bf44dacb6e7
Gracian Anton <gracian.anton@gmail.com>
2026-06-27 17:50:07 -0400

    commit

--- resources/js/UserStation.jsx
+++ resources/js/UserStation.jsx
@@ -4,12 +4,25 @@ import '../css/UserStation.css';
 export default function UserStation(){
     const data = window.__REACT_DATA__;
     console.log(data);
-    return ("Hello");
+    return (<Main />);
+}
+function convertArrayToObject(data){
+    const objectData = {};
+    let counter = -1
+    data.forEach(key => {
+        objectData.key = data[counter++]
+    });
+    return objectData;
 }
 function Main(){
     return (
         <div id='main'>
-
+            
         </div>
+    )
+}
+function station(){
+    return (
+        <div></div>
     )
 }
