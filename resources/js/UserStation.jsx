69dd0f8b97f330ac816c89dbbe3a7bd99c3b1c32
Gracian Anton <gracian.anton@gmail.com>
2026-06-28 21:33:31 -0400

    commit

--- resources/js/UserStation.jsx
+++ resources/js/UserStation.jsx
@@ -3,18 +3,19 @@ import '../css/UserStation.css';
 import { useState, useEffect } from "react";
 
 export default function UserStation({ data }){
-    return (<Main {...data}/>);
+    return (<Main {...data}/>); //sends data to Main component as object, not as property of object
 }
 function Main(station){
     console.log("Main:");
     console.log(station);
+    console.log(station.stationId)
     return (
         <div id='main'>
             <Station stationId = {station.stationId}/>
         </div>
     )
 }
-function Station(stationId){    
+function Station({stationId}){    
     console.log("Station Id: " + stationId);
 
     const [station, setStation] = useState([]);
