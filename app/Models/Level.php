a61720799305b143da2eeeee1414b86d0bad1475
Gracian Anton <gracian.anton@gmail.com>
2026-07-03 11:36:21 -0400

    commit

---
+++ app/Models/Level.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Level extends Model
+{
+    protected $table = 'levels';
+    public $timestamps = true;
+    // sets the fillable columns in the table
+    protected $fillable = [
+        'stationId',
+        'levels'
+    ];
+    // sets the data type for each fillable column
+    protected $casts = [
+        'stationId' => 'string',
+        'levels' => 'json'
+    ];
+
+}
