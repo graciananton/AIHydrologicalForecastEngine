171b20b3f391fa9c76dddea9d40708955c2fca5b
Gracian Anton <gracian.anton@gmail.com>
2026-07-03 14:09:18 -0400

    commit

--- app/Services/LevelService.php
+++ app/Services/LevelService.php
@@ -25,9 +25,19 @@ class LevelService{
             $rows = Http::get("https://api.weather.gc.ca/collections/hydrometric-daily-mean/items?STATION_NUMBER={$stationId}&f=json&limit=10000&filter=properties.LEVEL IS NOT NULL");
             $rows = json_decode($rows, true);
             $rows = $rows['features'];
+            
+            Log::channel("laravel")->info("Level-DATE");
             for($i = 0;$i<count($rows);$i++){
                 $row = $rows[$i];
-                $result = Level::firstOrCreate(['stationId' => $row['properties']['STATION_NUMBER'], 'levels' => $row['properties']['LEVEL'], 'time' => $row['properties']['DATE']]);                
+                Log::channel("laravel")->info($row['properties']['LEVEL']);
+                Log::channel("laravel")->info($row['properties']['DATE']);
+                $result = Level::firstOrCreate(
+                    [
+                        'stationId' => $row['properties']['STATION_NUMBER'], 
+                        'level' => $row['properties']['LEVEL'], 
+                        'time' => $row['properties']['DATE']
+                    ]
+                );                
             }
         }
 
