3baec5dbb95d43174cb7c2de4d0429f1f5a8c11f
Gracian Anton <gracian.anton@gmail.com>
2026-07-03 17:44:40 -0400

    commit

---
+++ app/Services/LevelAnalysisService.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Services;
+
+use App\Models\Level;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+
+class LevelAnalysisService{
+
+    public function normalizeParams(array $params): array{
+        return [
+            'stationId' => $params['stationId'] ?? '02KF001',
+            'time' => new Date($params['time']).toISOString() ?? new Date(0).toISOString(),
+            'level' => (string) $params['level'] ?? (string) 0.0
+        ];
+    }   
+
+    public function sync($stationId){
+        try{
+            $rows = Http::get("https://api.weather.gc.ca/collections/hydrometric-daily-mean/items?STATION_NUMBER={$stationId}&f=json&limit=10000&filter=properties.LEVEL IS NOT NULL");
+            
+        }
+
+        catch(Throwable $e){
+
+        }
+    }
+}
