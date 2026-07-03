7903d3cd93717106c07f5fddeff530e2ded6673e
Gracian Anton <gracian.anton@gmail.com>
2026-07-03 13:00:49 -0400

    commit

--- app/Services/LevelService.php
+++ app/Services/LevelService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 use App\Models\Level;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
 
 class LevelService{
 
@@ -16,5 +17,18 @@ class LevelService{
         $query = Level::query();
         $query->where('stationId', $params['stationId']);
         return $query->get()->toArray();
+    }
+
+    public function sync($stationId){
+        $query = Level::query();
+        $rows = Http::get("https://api.weather.gc.ca/collections/hydrometric-daily-mean/items?STATION_NUMBER={$stationId}&f=json&limit=10000&filter=properties.LEVEL IS NOT NULL");
+
+        for($i = 0;$i<count($data);$i++){
+            $row = $data[$i];
+            $result = User::firstOrCreate(['stationId' => $row['STATION_NUMBER'], 'level' => $row['LEVEL'], 'time' => $row['DATE']]);
+            // result stores the user if the row already exists
+            
+        }
+
     }
 }
