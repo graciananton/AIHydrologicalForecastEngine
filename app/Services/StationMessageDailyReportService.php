dec0a7354502dbb3e5576df9113918ed9f97bebc
Gracian Anton <gracian.anton@gmail.com>
2026-07-12 16:49:06 -0400

    commit

--- app/Services/StationMessageDailyReportService.php
+++ app/Services/StationMessageDailyReportService.php
@@ -18,19 +18,20 @@ class StationMessageDailyReportService{
             for($i=0;$i<count($users);$i++){
                 $user = $users[$i];
                 $stationId = $user['stationId'];
-                $url = "http://gracian.ca/laravel/public/api/stationMessage?stationId=".$stationId."&order=desc&limit=1";
+                $url = "http://gracian.ca/laravel/public/api/stationMessage?stationId=".$stationId."&order=desc&limit=1&role=user";
                 $stationMessage = Http::get($url);
                 $stationMessage = json_decode($stationMessage, true)[0];
                 $message = $stationMessage['message'];
 
                 $usersList[] = $user['name'];
-                
+
                 Mail::to($user['email'])->send(new StationMessageMail($message));
             }
 
            // $errors = ApplicationErrors::where('created_at', '>', Carbon::now()->subDay())->get()->toArray();
            // Mail::to("GracianAnton@cmail.carleton.ca")->send(new StationMessageMail($errors));
-            return response()->json("DailyReport sent for ". $usersList.join(", "));
+            $usersListStr = join(', ',$usersList);
+            return response()->json(["message" => "DailyReport sent for ". $usersListStr]);
         }
         catch(\Throwable $e){
             throw $e;
