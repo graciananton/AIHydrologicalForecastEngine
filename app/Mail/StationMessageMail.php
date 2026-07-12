a41eca858a06d3ba0088c0bd85ef8a1a41d43c8d
Gracian Anton <gracian.anton@gmail.com>
2026-07-12 15:51:50 -0400

    commit

---
+++ app/Mail/StationMessageMail.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Mail\Mailer;
+use Illuminate\Support\Facades\Log;
+
+class StationMessageMail extends Mailable
+{
+    use Queueable, SerializesModels;
+    private string $message;
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(string $message)
+    {
+        Log::channel("laravel")->info("OtpMail class initialization");
+        $this->message = $message;
+        // internally calls envelope(), content(), attachments() even though not called in __construct()
+    }
+
+    /**
+     * Get the message envelope.
+     * email headers (subject, from, reply-to)
+     */
+    public function envelope(): Envelope
+    {
+        Log::channel("laravel")->info("Creating envelope header details");
+        return new Envelope(
+            subject: 'AI Hydrological Forecasting Engine Message',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     * renders email content using otp blade file
+     */
+    public function content(): Content
+    {
+        Log::channel("laravel")->info("Creating content details");
+        return new Content(
+            view: 'emails.otp',
+            with: [
+                'otp'=> $this->otp
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        Log::channel("laravel")->info("Creating attachments details");
+        return [];
+    }
+    
+}
---
+++ resources/views/emails/stationMessage.blade.php
@@ -0,0 +1,11 @@
+<p>
+    Hello user,
+</p>
+<p>
+    <span style="font-size:0.8rem;">
+        <b> {{ $otp }} </b>
+    </span>
+</p>
+<p>
+    Thanks.
+</p>
