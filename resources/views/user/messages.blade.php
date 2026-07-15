661a71c0434425fd5edeb6a7f2b47141766d21f7
Gracian Anton <gracian.anton@gmail.com>
2026-07-15 12:30:15 -0400

    commit

--- resources/views/user/messages.blade.php
+++ resources/views/user/messages.blade.php
@@ -1,6 +1,10 @@
 <!doctype html>
 <html>
 <head>
+    @vite([
+        'resources/css/app.css',
+        'resources/js/app.jsx'
+    ])
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
 </head>
@@ -20,9 +24,5 @@
         window.__REACT_DATA__ = data;
     </script>
     <div id="react-root"></div>
-    @vite([
-        'resources/css/app.css',
-        'resources/js/app.jsx'
-    ])
 </body>
 </html>
--- resources/views/user/station.blade.php
+++ resources/views/user/station.blade.php
@@ -1,6 +1,10 @@
 <!doctype html>
 <html>
 <head>
+    @vite([
+        'resources/css/app.css',
+        'resources/js/app.jsx'
+    ])
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
 </head>
@@ -22,9 +26,5 @@
         window.__REACT_DATA__ = data;
     </script>
     <div id="react-root"></div>
-    @vite([
-        'resources/css/app.css',
-        'resources/js/app.jsx'
-    ])
 </body>
 </html>
