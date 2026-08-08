6e72e746bf8dcddcdebd3cbb65025daaa754bfc6
Gracian Anton <gracian.anton@gmail.com>
2026-08-08 00:21:58 -0400

    commit

--- app/Console/Commands/StationMessageDailyReportScheduler.php
+++ app/Console/Commands/StationMessageDailyReportScheduler.php
@@ -11,7 +11,7 @@ class StationMessageDailyReport extends Command
                             
     public function handle()
     {        
-        Log::channel("weather")->info("hanldign station message dialy report");
+        Log::channel("weather")->info("handling station message dialy report");
         $response = Http::timeout(1200)->get(
             'https://gracian.ca/laravel/public/api/stationMessageDailyReport',
         );
